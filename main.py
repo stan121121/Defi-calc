32cc7b4999506d6078b75e8b447b59036e81a49d
stan121121 <taponni.ps.store@gmail.com>
2026-01-31 12:04:49 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
 Enter "help" below or click "Help" above for more information.
 >>> import asyncio
+... import os
 ... from aiogram import Bot, Dispatcher, types
 ... from aiogram.filters import Command
 ... from aiogram.fsm.state import StatesGroup, State
 ... from aiogram.fsm.context import FSMContext
 ... from aiogram.fsm.storage.memory import MemoryStorage
 ... from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-... import os
-... TOKEN = os.getenv "8304350198:AAF8hWMzi2IlPuAi3td0AM9O-DmrsjaI6vg"
+... TOKEN = os.getenv "BOT_TOKEN"
 ... 
 ... bot = Bot(token=TOKEN)
 ... dp = Dispatcher(storage=MemoryStorage())
