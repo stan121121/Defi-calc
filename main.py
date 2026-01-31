0c1a53502189b4785fcc7969888a7f2438681f93
stan121121 <taponni.ps.store@gmail.com>
2026-01-31 12:08:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
 Enter "help" below or click "Help" above for more information.
 >>> import asyncio
-... import os
+>>> import os
 ... from aiogram import Bot, Dispatcher, types
 ... from aiogram.filters import Command
 ... from aiogram.fsm.state import StatesGroup, State
@@ -159,3 +159,4 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+
